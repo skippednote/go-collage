785d248c0aa229580b4f71262c48cba48bf9ddc8
Bassam Ismail <skippednote@gmail.com>
2020-08-22 18:19:49 +0530

    render image

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,42 @@
 const Home = () => {
-  const submit = (e) => {
+  const [src, setSrc] = useState();
+  const [err, setErr] = useState();
+
+  const submit = async (e) => {
     e.preventDefault();
     let [uri, regex] = e.currentTarget.elements;
     uri = uri.value;
     regex = regex.value.replace(/\\/, "\\\\");
 
-    fetch("/api/", {
-      method: "POST",
-      body: JSON.stringify({
-        uri,
-        regex,
-      }),
-    }).then((r) => r.blob());
+    try {
+      const collageBlob = await fetch("/api/", {
+        method: "POST",
+        body: JSON.stringify({
+          uri,
+          regex,
+        }),
+      }).then((r) => r.blob());
+      setSrc(URL.createObjectURL(collageBlob));
+    } catch (e) {
+      setErr(e.message);
+      console.log(e);
+    }
   };
   return (
     <main>
       <h1>Home</h1>
+      {err && <h4>{err}</h4>}
       <form onSubmit={submit}>
         <input placeholder="url" />
         <input placeholder="image regex" />
         <button type="submit">Submit</button>
       </form>
+      {src && (
+        <>
+          <hr />
+          <img src={src} />
+        </>
+      )}
     </main>
   );
 };
