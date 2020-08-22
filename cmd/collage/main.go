0c8db1746287d42a35ed6b6fdc680007a145e9b8
Bassam Ismail <skippednote@gmail.com>
2020-08-22 20:17:41 +0530

    move main to cmd

--- cmd/collage/main.go
+++ cmd/collage/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/pkg/profile"
 	"github.com/skippednote/go-collage/route"
 )
 
 var Handler = route.Handler
 
 func main() {
-	defer profile.Start().Stop()
+	// defer profile.Start().Stop()
 	http.HandleFunc("/", Handler)
 	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/skippednote/go-collage
 
 go 1.14
 
-require (
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
-	github.com/pkg/profile v1.5.0
-)
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
